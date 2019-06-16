c78f7e4fe3230c629ea55bc7f30256c208a473fd
Andy Foston <afoston@ukcloud.com>
2019-06-16 20:38:59 +0100

    Removing incorrect licence from setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from distutils.core import setup
 setup(
     name='CodingKata',
     version='0.1dev',
-    license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
     tests_require=['pylint', 'coverage'],
     scripts=['basic_arithmetic.py',
